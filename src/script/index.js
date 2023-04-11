91a20d97b22fdee130ad0b2c3931c94eff188ade
GhafickyRenku <mghaficky.r@gmail.com>
2023-04-11 18:36:28 +0700

    final

---
+++ src/script/index.js
@@ -0,0 +1,63 @@
+import { getCountries, getHistoryContry } from "./api.js";
+import { format_date, loader, formatRibuan } from "./functions.js";
+
+const loading = document.getElementById('loader')
+loading.innerHTML = loader();
+const date = new Date();
+const today = document.getElementById('today')
+today.innerHTML = format_date(date)
+const countries = await getCountries();
+const dataCountries = countries.response;
+const selectCountries = document.getElementById('countries');
+const country = document.getElementById('country')
+
+// number total
+const activecase = document.getElementById('active-case');
+const newcase = document.getElementById('new-case');
+const recoveredcase = document.getElementById('recovered-case');
+const totalcases = document.getElementById('total-cases');
+const totaldeaths =document.getElementById('total-deaths');
+const totaltests = document.getElementById('total-tests');
+const populasi = document.getElementById('populasi');
+
+
+loading.innerHTML = '';
+dataCountries.forEach(element => {
+    selectCountries.innerHTML += `
+        <option value="${element}">${element}</option>
+    `
+});
+
+
+
+
+selectCountries.addEventListener('change', async function(e){
+    e.preventDefault()
+
+    activecase.innerHTML = 'Getting data⌛';
+    newcase.innerHTML = 'Getting data⌛';
+    recoveredcase.innerHTML = 'Getting data⌛';
+    totalcases.innerHTML = 'Getting data⌛';
+    totaldeaths.innerHTML = 'Getting data⌛';
+    totaltests.innerHTML = 'Getting data⌛';
+    populasi.innerHTML = 'Getting data⌛'
+
+    const countryselect = selectCountries.value
+    country.innerHTML = `- ${countryselect}`
+    const today = `${date.getFullYear()}-0${date.getMonth()+1}-0${date.getDate()}`;
+    const result = await getHistoryContry(countryselect, today);
+    
+    const history_country = result.response[0];
+    const caseCountry = history_country.cases;
+    const deathsCountry = history_country.deaths;
+    const testsCountry = history_country.tests;
+
+    activecase.innerHTML = (caseCountry.active) ? formatRibuan(caseCountry.active.toString()) : 0;
+    newcase.innerHTML = (caseCountry.new) ? caseCountry.new : 0;
+    recoveredcase.innerHTML = (caseCountry.recovered) ? formatRibuan(caseCountry.recovered.toString()): 0;
+    totalcases.innerHTML = (caseCountry.total) ? formatRibuan(caseCountry.total.toString()) : 0;
+    totaldeaths.innerHTML = `${(deathsCountry.total) ? formatRibuan(deathsCountry.total.toString()) : 0} (${(deathsCountry.new)? deathsCountry.new : 0})`;
+    totaltests.innerHTML = (testsCountry.total) ? formatRibuan(testsCountry.total.toString()) : 0;
+    populasi.innerHTML = (history_country.population) ? formatRibuan(history_country.population.toString()) : 0;
+    
+})
